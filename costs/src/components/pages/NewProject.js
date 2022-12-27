30259bc27210a4661a239c58e2073c2a94d98996
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-12-27 09:58:57 -0300

    fix: fetch api

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,14 +1,34 @@
-import ProjectForm from "../project/ProjectForm"
-import "./NewProjectStyle.css"
+import { json, useNavigate } from "react-router-dom"; 
 
-function NewProject (){
-    return(
-        <div className="newProjectContainer">
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto"></ProjectForm>
-        </div>
-    )
+import ProjectForm from "../project/ProjectForm";
+import "./NewProjectStyle.css";
+
+function NewProject() {
+  const history = useNavigate(); 
+
+  function creatPost(project){
+    project.cost = 0
+    project.services = []
+
+    fetch('http://localhost:5000/projects', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(project) 
+    })
+    .then(resp => resp.json())
+    .then(data => {console.log(data)})
+    .catch(erro => console.log(erro))
+  }
+
+  return (
+    <div className="newProjectContainer">
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={creatPost} btnText="Criar Projeto"></ProjectForm>
+    </div>
+  );
 }
 
-export default NewProject
+export default NewProject;
--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -5,32 +5,43 @@ import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
-function ProjectForm({ btnText }) {
-  let [categories, setCategories] = useState([]);
+function ProjectForm({ handleSubmit, btnText, projectData }) {
+  const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   useEffect(() => {
     // Pegando direto do 'Banco de dados'
-    fetch("http://localhost:5000/categories", {
+    fetch("http://localhost:5000/categories ", {
       method: "GET",
-      headres: {
+      headers: {
         "Content-Type": "application/json",
       },
     })
       .then((resp) => resp.json())
       .then((data) => {
-        setCategories = data;
+        setCategories(data);
       })
       .catch((erro) => console.log(erro));
   }, []);
   // Pegando direto do 'Banco de dados'
 
+  const submit = (e) => {
+    e.preventDefault();
+    handleSubmit(project);
+  };
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  }
+
   return (
-    <form className="form">
+    <form onSubmit={submit} className="form">
       <Input
         type="text"
         txt="Nome do projeto"
         name="name"
         placeholder="Insira o nome do projeto"
+        handleOnChange={handleChange}
       ></Input>
 
       <Input
@@ -38,6 +49,7 @@ function ProjectForm({ btnText }) {
         txt="Orçamento do projeto"
         name="budget"
         placeholder="Insira o orçamento total"
+        handleOnChange={handleChange}
       ></Input>
 
       <Select
